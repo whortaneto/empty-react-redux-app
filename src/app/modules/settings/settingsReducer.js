c4723db068cbf06eab19ef251d2d15a4a520c121
Wagner Horta <whorta@guidewire.com>
2019-04-16 16:32:49 +0100

    removing immutableJS and using immer

--- src/app/modules/settings/settingsReducer.js
+++ src/app/modules/settings/settingsReducer.js
@@ -12,9 +12,8 @@ const initialState = {
 export const reducer = produce((state = initialState, action) => {
   switch (action.type) {
     case actions.SETTINGS_SET: {
-      // eslint-disable-next-line
-      state = action.payload;
       state.hasServerChanged = state.server !== action.payload.server;
+      state.server = action.payload.server;
       break;
     }
     default:
